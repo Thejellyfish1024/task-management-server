6019784c257fc506b2c3308184c3c4168f233807
Thejellyfish1024 <alamin102410@gmail.com>
2023-12-24 17:21:33 +0600

    made delete method for tasks

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 // 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s79pxyc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -41,10 +41,17 @@ async function run() {
 
     app.get('/all-tasks/:email', async(req,res) =>{
         const email = req.params.email;
-        console.log(email);
+        // console.log(email);
         const query = { email : email};
         const result = await taskCollection.find(query).toArray();
         res.send(result);
+    })
+
+    app.delete('/task/:id', async(req,res) =>{
+        const id = req.params.id;
+        const query = { _id : new ObjectId(id)}
+        const result = await taskCollection.deleteOne(query)
+        res.send(result)
     })
 
 
